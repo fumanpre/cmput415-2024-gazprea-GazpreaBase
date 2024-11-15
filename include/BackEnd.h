e4f48c7e590effaeb8061631325dc646d4c9c0b1
Fuman <fumanpre@ualberta.ca>
2024-11-14 17:51:59 -0700

    made changes in backend.h and cpp

--- include/BackEnd.h
+++ include/BackEnd.h
@@ -1,14 +1,52 @@
 #pragma once
 
+#include "Ast.h"
+#include "AstWalker.h"
+
+#include "Symbols.h"
+#include "Scoping.h"
+
+// Pass manager
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Conversion/SCFToControlFlow/SCFToControlFlow.h"
+#include "mlir/Conversion/ControlFlowToLLVM/ControlFlowToLLVM.h"
+#include "mlir/Conversion/ArithToLLVM/ArithToLLVM.h"
+#include "mlir/Conversion/MemRefToLLVM/MemRefToLLVM.h"
+#include "mlir/Conversion/FuncToLLVM/ConvertFuncToLLVM.h"
+#include "mlir/Conversion/ReconcileUnrealizedCasts/ReconcileUnrealizedCasts.h"
+
+// Translation
+#include "mlir/Target/LLVMIR/Dialect/LLVMIR/LLVMToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Dialect/Builtin/BuiltinToLLVMIRTranslation.h"
+#include "mlir/Target/LLVMIR/Export.h"
+#include "llvm/Support/raw_os_ostream.h"
+
+// MLIR IR
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/TypeRange.h"
+#include "mlir/IR/Value.h"
+#include "mlir/IR/ValueRange.h"
+#include "mlir/IR/Verifier.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/BuiltinOps.h"
+
+// Dialects 
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/ControlFlow/IR/ControlFlow.h"
+
 
 class BackEnd {
  public:
     BackEnd();
 
     int emitMain();
+    int lowerDialects();
+    void dumpLLVM(std::ostream &os);
 
  protected:
     void setupPrintf();
@@ -20,4 +58,20 @@ class BackEnd {
     std::shared_ptr<mlir::OpBuilder> builder;
 
     mlir::Location loc;
+
+   // holder for value 0 and 1
+    mlir::Value constOne;
+    mlir::Value constZero;
+    mlir::Value constMax;
+
+    // Types
+    mlir::Type intTy, ptrTy;
+    mlir::MemRefType memreftype;
+
+   // ast walking methods
+   std::any visit(std::shared_ptr<AST> t) ;
+   std::any visitVAR_DECL(std::shared_ptr<AST> t) ;
+   std::any visitASSIGN(std::shared_ptr<AST> t);
+
+
 };
--- src/BackEnd.cpp
+++ src/BackEnd.cpp
@@ -1,3 +1,4 @@
+
 #include <assert.h>
 
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
@@ -9,15 +10,42 @@
 #include "BackEnd.h"
 
 BackEnd::BackEnd() : loc(mlir::UnknownLoc::get(&context)) {
+    // Load Dialects.
     context.loadDialect<mlir::LLVM::LLVMDialect>();
+    context.loadDialect<mlir::arith::ArithDialect>();
+    context.loadDialect<mlir::scf::SCFDialect>();
+    context.loadDialect<mlir::cf::ControlFlowDialect>();
+    context.loadDialect<mlir::memref::MemRefDialect>(); 
 
+    // Initialize the MLIR context 
     builder = std::make_shared<mlir::OpBuilder>(&context);
 
     // Open a new context and module.
     module = mlir::ModuleOp::create(builder->getUnknownLoc());
     builder->setInsertionPointToStart(module.getBody());
 
+    // initialize types
+    intTy = builder->getI32Type();
+    ptrTy = mlir::LLVM::LLVMPointerType::get(&context);
+    memreftype = mlir::MemRefType::get({mlir::ShapedType::kDynamic}, intTy);
+
     setupPrintf();
+}
+
+void BackEnd::printInt(mlir::Value integer) {
+    mlir::LLVM::GlobalOp formatString;
+    if (!(formatString = module.lookupSymbol<mlir::LLVM::GlobalOp>("intFormat"))) {
+        llvm::errs() << "missing format string!\n";
+    }
+    
+    mlir::Value formatStringPtr = builder->create<mlir::LLVM::AddressOfOp>(loc, formatString);
+    
+    mlir::ValueRange args = {formatStringPtr, integer}; 
+    
+    mlir::LLVM::LLVMFuncOp printfFunc = module.lookupSymbol<mlir::LLVM::LLVMFuncOp>("printf");
+    
+    builder->create<mlir::LLVM::CallOp>(loc, printfFunc, args);
+    
 }
 
 int BackEnd::emitMain() {
@@ -29,6 +57,11 @@ int BackEnd::emitMain() {
 
     printNewline();
 
+    // Init constants
+    constZero = builder->create<mlir::arith::ConstantIntOp>(loc, 0, intTy); 
+    constOne = builder->create<mlir::arith::ConstantIntOp>(loc, 1, intTy);
+    constMax = builder->create<mlir::arith::ConstantIntOp>(loc, 2147483647, intTy);
+
     mlir::Value zero = builder->create<mlir::LLVM::ConstantOp>(loc, intType, builder->getIntegerAttr(intType, 0));
     builder->create<mlir::LLVM::ReturnOp>(builder->getUnknownLoc(), zero);
 
@@ -39,6 +72,45 @@ int BackEnd::emitMain() {
         return -1;
     }
     return 0;
+}
+
+int BackEnd::lowerDialects() {
+    // Set up the MLIR pass manager to iteratively lower all the Ops
+    mlir::PassManager pm(&context);
+
+    // Lower SCF to CF (ControlFlow)
+    pm.addPass(mlir::createConvertSCFToCFPass());
+
+    // Lower Arith to LLVM
+    pm.addPass(mlir::createArithToLLVMConversionPass());
+
+    // Lower MemRef to LLVM
+    pm.addPass(mlir::createFinalizeMemRefToLLVMConversionPass());
+
+    // Lower CF to LLVM
+    pm.addPass(mlir::createConvertControlFlowToLLVMPass());
+
+    // Finalize the conversion to LLVM dialect
+    pm.addPass(mlir::createReconcileUnrealizedCastsPass());
+
+    // Run the passes
+    if (mlir::failed(pm.run(module))) {
+        llvm::errs() << "Pass pipeline failed\n";
+        return 1;
+    }
+    return 0;
+}
+
+void BackEnd::dumpLLVM(std::ostream &os) {  
+    // The only remaining dialects in our module after the passes are builtin
+    // and LLVM. Setup translation patterns to get them to LLVM IR.
+    mlir::registerBuiltinDialectTranslation(context);
+    mlir::registerLLVMDialectTranslation(context);
+    llvm_module = mlir::translateModuleToLLVMIR(module, llvm_context);
+
+    // Create llvm ostream and dump into the output file
+    llvm::raw_os_ostream output(os);
+    output << *llvm_module;
 }
 
 void BackEnd::setupPrintf() {
